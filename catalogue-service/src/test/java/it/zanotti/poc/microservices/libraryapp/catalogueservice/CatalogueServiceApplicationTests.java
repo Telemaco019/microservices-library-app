b20a1ce868aa4605219c5cb2d1c5e09c9505905c
Michele Zanotti <m.zanotti@myti.it>
2020-12-24 11:35:41 +0100

    Init project

---
+++ catalogue-service/src/test/java/it/zanotti/poc/microservices/libraryapp/catalogueservice/CatalogueServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package it.zanotti.poc.microservices.libraryapp.catalogueservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogueServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
