dc74e92e6b2c41a8038ce0e20691b575aa3bff61
Michele Zanotti <m.zanotti@myti.it>
2020-12-25 21:56:42 +0100

    Refactor KafkaTopicConfig

--- catalogue-service/src/main/java/it/zanotti/poc/microservices/libraryapp/catalogueservice/config/KafkaTopicConfig.java
+++ catalogue-service/src/main/java/it/zanotti/poc/microservices/libraryapp/catalogueservice/config/KafkaTopicConfig.java
@@ -2,33 +2,20 @@ package it.zanotti.poc.microservices.libraryapp.catalogueservice.config;
 
 import it.zanotti.poc.microservices.libraryapp.commons.AppConsts;
 import org.apache.kafka.clients.admin.NewTopic;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.core.KafkaAdmin;
-
-import java.util.HashMap;
+import org.springframework.kafka.config.TopicBuilder;
 
 /**
  * @author Michele Zanotti on 25/12/20
  **/
 @Configuration
 public class KafkaTopicConfig {
-    private final KafkaProperties kafkaProperties;
-
-    @Autowired
-    public KafkaTopicConfig(KafkaProperties kafkaProperties) {
-        this.kafkaProperties = kafkaProperties;
-    }
-
-    @Bean
-    public KafkaAdmin kafkaAdmin() {
-        return new KafkaAdmin(new HashMap<>(kafkaProperties.buildAdminProperties()));
-    }
-
     @Bean
     public NewTopic booksTopic() {
-        return new NewTopic(AppConsts.TOPIC_BOOKS, 1, (short) 1);
+        return TopicBuilder.name(AppConsts.TOPIC_BOOKS)
+                .partitions(1)
+                .replicas(1)
+                .build();
     }
 }
