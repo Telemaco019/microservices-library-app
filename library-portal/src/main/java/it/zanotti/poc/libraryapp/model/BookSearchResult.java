acb02b95c8d38dc6d63a1dbc432b63d2946652b9
Michele Zanotti <m.zanotti@myti.it>
2021-01-02 10:13:28 +0100

    Fix NPE

--- library-portal/src/main/java/it/zanotti/poc/libraryapp/model/BookSearchResult.java
+++ library-portal/src/main/java/it/zanotti/poc/libraryapp/model/BookSearchResult.java
@@ -3,9 +3,11 @@ package it.zanotti.poc.libraryapp.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import org.apache.commons.compress.utils.Lists;
+import org.apache.commons.lang3.StringUtils;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * @author Michele Zanotti on 31/12/20
@@ -20,10 +22,13 @@ public class BookSearchResult {
     private String bookSubtitle;
     @JsonProperty("description")
     private String bookDescription;
+    @JsonProperty("authors")
     private List<String> bookAuthors = Lists.newArrayList();
     private LocalDate publicationDate;
 
     public String getAuthorsAsString() {
-        return String.join(", ", bookAuthors);
+        return Objects.isNull(bookAuthors) ?
+                StringUtils.EMPTY :
+                String.join(", ", bookAuthors);
     }
 }
