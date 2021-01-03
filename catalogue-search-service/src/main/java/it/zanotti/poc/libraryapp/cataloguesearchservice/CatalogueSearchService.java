d227509db243b64f25ffb48da410abff9ff288b5
Michele Zanotti <m.zanotti@myti.it>
2021-01-03 21:50:35 +0100

    Register catalogue-service and catalogue-search-service to Eureka server

--- catalogue-search-service/src/main/java/it/zanotti/poc/libraryapp/cataloguesearchservice/CatalogueSearchService.java
+++ catalogue-search-service/src/main/java/it/zanotti/poc/libraryapp/cataloguesearchservice/CatalogueSearchService.java
@@ -2,10 +2,12 @@ package it.zanotti.poc.libraryapp.cataloguesearchservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author Michele Zanotti on 25/12/20
  **/
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CatalogueSearchService {
     public static void main(String[] args) {
