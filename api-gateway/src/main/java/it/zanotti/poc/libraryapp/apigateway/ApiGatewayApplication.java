a2293ae4cac2dfa97428e8a08e619cf091d3b73a
Michele Zanotti <m.zanotti@myti.it>
2021-01-03 22:24:16 +0100

    Make api-gateweay use Eureka registry

--- api-gateway/src/main/java/it/zanotti/poc/libraryapp/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/it/zanotti/poc/libraryapp/apigateway/ApiGatewayApplication.java
@@ -3,7 +3,9 @@ package it.zanotti.poc.libraryapp.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ApiGatewayApplication {
   public static void main(String[] args) {
